ad947c6c9b1012f80625c03601b147611b572588
Daniel Peña Perez <danipenaperez@gmail.com>
2021-11-11 08:20:00 +0100

    delete cache

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return true;
-        }).map(function(cacheName) {
-          console.log("deleting cache "+ cacheName);
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
+// self.addEventListener('activate', function(event) {
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames.filter(function(cacheName) {
+//           return true;
+//         }).map(function(cacheName) {
+//           console.log("deleting cache "+ cacheName);
+//           return caches.delete(cacheName);
+//         })
+//       );
+//     })
+//   );
+// });
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
